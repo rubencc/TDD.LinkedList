09cb2eb38fd0e9a7378df775c7e1ac1f23951970
Rubén C.C <ruben.carretero@wolterskluwer.com>
2017-02-21 17:26:21 +0100

    Add project files.

---
+++ TDD.LinkedList/Entities/LinkedList.cs
@@ -0,0 +1,53 @@
+﻿namespace TDD.LinkedList
+{
+    public sealed class LinkedList<T>
+        where T : class
+    {
+        private Node<T> head;
+
+        public LinkedList()
+        {
+            this.head = null;
+        }
+
+        public void Push(T data)
+        {
+            
+        }
+
+        public void PushAfter(Node<T> node, T data)
+        {
+
+        }
+
+        public void PushBefore(Node<T> node, T data)
+        {
+
+        }
+
+        public T Pop()
+        {
+            return null;
+        }
+
+        public T Pop(int index)
+        {
+            return null;
+        }
+
+        public void Remove(T data)
+        {
+            
+        }
+
+        public int Count()
+        {
+            return 0;
+        }
+
+        public void Clear()
+        {
+            
+        }
+    }
+}
---
+++ TDD.LinkedList/Entities/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TDD.LinkedList
+{
+    public sealed class Node<T> where T: class
+    {
+        private Node<T> next;
+        public T Content { get; set; }
+
+        public Node(T data)
+        {
+            if (data == null)
+                throw new ArgumentNullException();
+            this.Content = data;
+        }
+
+        public void SetNext(Node<T> node)
+        {
+            if(node != null)
+                this.next = node;
+        }
+
+        public Node<T> GetNext()
+        {
+            return this.next;
+        }
+    }
+}
