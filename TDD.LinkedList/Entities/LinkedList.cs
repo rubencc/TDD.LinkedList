bc1b9514d858f50ed402d9b65efba712f09c1728
Cesar Marin <cesarmj1974@gmail.com>
2017-03-03 11:11:17 +0100

    First Test

--- TDD.LinkedList/Entities/LinkedList.cs
+++ TDD.LinkedList/Entities/LinkedList.cs
@@ -7,6 +7,20 @@ namespace TDD.LinkedList
     public sealed class LinkedList<T>
         where T : class
     {
-        
+       
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Node<T> Get()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
